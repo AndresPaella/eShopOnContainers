4dfefa54eea65b3abea652046882795176d14ae5
EPS <EPS@clLS13I-24>
2021-11-17 17:46:00 +0100

    Creación de clases

---
+++ src/Services/Catalog-API/Model/CatalogItem.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog_API.Model
+{
+    public class CatalogItem
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
+
+        [ForeignKey("CatalogTypeId")]
+        public CatalogType CatalogType { get; set; }
+
+        [ForeignKey("CatalogBrandId")]
+        public CatalogBrand CatalogBrand { get; set; }
+        public int AvailableStock { get; set; }
+
+        public int RestockThreshold { get; set; }
+
+        public int MaxStockThreshold { get; set; }
+
+        /// <summary>
+        /// True if item is on reorder
+        /// </summary>
+        public bool OnReorder { get; set; }
+
+        public CatalogItem() { }
+    }
+}
---
+++ src/Services/Catalog-API/Model/CatalogType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog_API.Model
+{
+    public class CatalogType
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string Type { get; set; }
+    }
+
+}
